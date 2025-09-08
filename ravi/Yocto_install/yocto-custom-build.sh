0618b42049f91fca8881f12a01a2f81bb6f72f66
om <om.rathod@einfochips.com>
2025-09-08 14:45:59 +0530

    Initial install yocto script

--- ravi/Yocto_install/yocto-custom-build.sh
+++ ravi/Yocto_install/yocto-custom-build.sh
@@ -3,32 +3,14 @@
 set -eu
 
 # Configuration
-YOCTO_DIR="$PWD/yocto"
+YOCTO_DIR="$HOME/yocto"
 POKY_DIR="$YOCTO_DIR/poky"
 BUILD_DIR="$YOCTO_DIR/build"
 BRANCH="kirkstone"
-LOG_FILE="logs/Yocto_install_add_recipe_$(date +%d_%m_%y_%H_%M_%S).log"
+
+LOG_FILE="logs/Yocto_install_and_build_$(date +%d_%m_%y_%H_%M_%S).log"
 
 exec > >(tee -a "$LOG_FILE") 2>&1
-
- #Create directory structure
-if [ -d "$YOCTO_DIR" ]; then
-        echo -e "Directory already exists\n\tif you want to overwrite press 1 or if you want exit press any key: " 
-        read choice
-        echo "User input: $choice" 
-        if [ "$choice" == "1" ]; then
-                echo "overwriting"      
-        else
-                echo "safely exiting"
-                exit
-        fi
-else
-        mkdir -p "$YOCTO_DIR"
-        echo "creating Directory"
-fi
-
-
-
 
 # Install required packages
 sudo apt update
@@ -52,6 +34,8 @@ echo "Verifying locale settings..."
 locale
 
 
+# Create directory structure
+mkdir -p "$YOCTO_DIR"
 #cd "$YOCTO_DIR"
 
 # Clone Poky if not already present
@@ -68,51 +52,21 @@ source "$POKY_DIR/oe-init-build-env" "$BUILD_DIR"
 set -u
 
 # Disable LTO to prevent gcc-cross build issues
-#echo 'INHERIT_REMOVE = "lto"' >> "$BUILD_DIR/conf/local.conf"
+echo 'INHERIT_REMOVE = "lto"' >> "$BUILD_DIR/conf/local.conf"
 
 echo "================================================================="
 echo "=============== To enter the build environment later, run : source poky/oe-init-build-env build ==============="
 echo "=============== Setup is Done. ==============="
 echo "================================================================="
-echo "=============== Creating Creating Layer  ==============="
-
-#cd ../
-
-if [ ! -d "$BUILD_DIR/meta-jsonapp" ]; then
-	bitbake-layers create-layer meta-jsonapp
-	bitbake-layers add-layer meta-jsonapp
-fi
-
-
-echo "=============== Creating cloning recipes from git  ==============="
-cd $BUILD_DIR/meta-jsonapp/
-
-if [ ! -d recipes-jsonapp ]; then
-	git clone https://github.com/rathod-om/Practice_repo
-
-	cp -r $BUILD_DIR/meta-jsonapp/Practice_repo/ravi/recipes-jsonapp/ .
-	
-	rm -rf $BUILD_DIR/meta-jsonapp/Practice_repo  
-fi
-
-echo "=============== bitbaking the core-image-cjson code  ==============="
-
-#cd $BUILD_DIR/meta-jsonapp/recipes-jsonapp/cjsonlib/
-#pwd
-#bitbake cjsonlib
-
-#cd $BUILD_DIR/meta-jsonapp/recipes-jsonapp/cjsoncode/
-#bitbake cjsoncode
-
-#cd $BUILD_DIR/meta-jsonapp/recipes-jsonapp/image/
-
-MACHINE=beaglebone-yocto bitbake core-image-cjson
-
 
 
 # Creating Image for beagle-bone-black
-#echo "=============== Creating Image for beagle-bone-black  ==============="
-#MACHINE=beaglebone-yocto bitbake core-image-minimal
+echo "=============== Creating Image for beagle-bone-black  ==============="
+
+set +u
+source "$POKY_DIR/oe-init-build-env" "$BUILD_DIR"
+set -u
+
+MACHINE=beaglebone-yocto bitbake core-image-minimal
 #/home/omrathod/Yocto_install/create_image.sh
-echo "=============== Image built for cjson code  ==============="
 
